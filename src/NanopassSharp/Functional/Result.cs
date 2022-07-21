de6847fff0f3d5521339d50e5cc79f72d9d4172f
thinker227 <thinker0227@gmail.com>
2022-07-21 16:35:30 +0200

    Organize Result extensions

--- src/NanopassSharp/Functional/Result.cs
+++ src/NanopassSharp/Functional/Result.cs
@@ -100,13 +100,6 @@ public readonly struct Result<T> {
 		if (IsSuccess) await ifSuccess(Value);
 		else await ifFailure(Error);
 	}
-	/// <summary>
-	/// Returns the inner value as either itself or the type's default value.
-	/// </summary>
-	public T? ToMaybeDefault() =>
-		IsSuccess
-			? Value
-			: default;
 
 	/// <summary>
 	/// Maps the inner value of the result.
@@ -176,7 +169,7 @@ public static class Result {
 	/// </summary>
 	/// <param name="value">The value of the result.</param>
 	/// <param name="error">The error message is <paramref name="error"/> is null.</param>
-	public static Result<T> CreateNotNull<T>(T? value, string? error = null, [CallerArgumentExpression(nameof(value))] string callerExpression = "") where T : class =>
+	public static Result<T> CreateNotNull<T>(T? value, string? error = null, [CallerArgumentExpression("value")] string callerExpression = "") where T : class =>
 		value is not null
 			? new(value)
 			: new(error ?? $"{callerExpression} was null");
@@ -185,114 +178,9 @@ public static class Result {
 	/// </summary>
 	/// <param name="value">The value of the result.</param>
 	/// <param name="error">The error message is <paramref name="error"/> is null.</param>
-	public static Result<T> CreateNotNull<T>(T? value, string? error = null, [CallerArgumentExpression(nameof(value))] string callerExpression = "") where T : struct =>
+	public static Result<T> CreateNotNull<T>(T? value, string? error = null, [CallerArgumentExpression("value")] string callerExpression = "") where T : struct =>
 		value is not null
 			? new(value.Value)
 			: new(error ?? $"{callerExpression} was null");
-
-}
-
-public static class ResultExtensions {
-
-	public static async Task<TResult> SwitchResultAsync<T, TResult>(this Task<Result<T>> source, Func<T, Task<TResult>> ifSuccess, Func<string, Task<TResult>> ifFailure) =>
-		await (await source).SwitchAsync(ifSuccess, ifFailure);
-	public static async Task<TResult> SwitchResultAsync<T, TResult>(this Task<Result<T>> source, Func<T, TResult> ifSuccess, Func<string, TResult> ifFailure) =>
-		(await source).Switch(ifSuccess, ifFailure);
-	
-	public static async Task<T?> ToMaybeDefaultAsync<T>(this Task<Result<T>> source) =>
-		(await source).ToMaybeDefault();
-
-	/// <summary>
-	/// Asynchronously maps the inner value of the result of a task evaluating to a <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to map to.</typeparam>
-	/// <param name="f">The transformer function to execute on the inner value.</param>
-	/// <returns>A new <see cref="Result{T}"/> with the inner value mapped if the source result was a success.</returns>
-	public static async Task<Result<U>> MapResultAsync<T, U>(this Task<Result<T>> source, Func<T, Task<U>> f) =>
-		await (await source).MapAsync(f);
-	/// <summary>
-	/// Asynchronously maps the inner value of the result of a value task evaluating to a <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to map to.</typeparam>
-	/// <param name="f">The transformer function to execute on the inner value.</param>
-	/// <returns>A new <see cref="Result{T}"/> with the inner value mapped if the source result was a success.</returns>
-	public static async Task<Result<U>> MapResultAsync<T, U>(this ValueTask<Result<T>> source, Func<T, Task<U>> f) =>
-		await (await source).MapAsync(f);
-	/// <summary>
-	/// Asynchronously maps the inner value of the result of a task evaluating to a <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to map to.</typeparam>
-	/// <param name="f">The transformer function to execute on the inner value.</param>
-	/// <returns>A new <see cref="Result{T}"/> with the inner value mapped if the source result was a success.</returns>
-	public static async Task<Result<U>> MapResultAsync<T, U>(this Task<Result<T>> source, Func<T, U> f) =>
-		(await source).Map(f);
-	/// <summary>
-	/// Asynchronously maps the inner value of the result of a value task evaluating to a <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to map to.</typeparam>
-	/// <param name="f">The transformer function to execute on the inner value.</param>
-	/// <returns>A new <see cref="Result{T}"/> with the inner value mapped if the source result was a success.</returns>
-	public static async Task<Result<U>> MapResultAsync<T, U>(this ValueTask<Result<T>> source, Func<T, U> f) =>
-		(await source).Map(f);
-	/// <summary>
-	/// Asynchronously binds the inner value of the result of a task evaluating to a <see cref="Result{T}"/> to a new <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to bind to.</typeparam>
-	/// <param name="f">The transformer function returning a new result to execute on the inner value.</param>
-	/// <returns>The <see cref="Result{T}"/> returned by <paramref name="f"/>.</returns>
-	public static async Task<Result<U>> BindResultAsync<T, U>(this Task<Result<T>> source, Func<T, Task<Result<U>>> f) =>
-		await (await source).BindAsync(f);
-	/// <summary>
-	/// Asynchronously binds the inner value of the result of a value task evaluating to a <see cref="Result{T}"/> to a new <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to bind to.</typeparam>
-	/// <param name="f">The transformer function returning a new result to execute on the inner value.</param>
-	/// <returns>The <see cref="Result{T}"/> returned by <paramref name="f"/>.</returns>
-	public static async Task<Result<U>> BindResultAsync<T, U>(this ValueTask<Result<T>> source, Func<T, Task<Result<U>>> f) =>
-		await (await source).BindAsync(f);
-	/// <summary>
-	/// Asynchronously binds the inner value of the result of a task evaluating to a <see cref="Result{T}"/> to a new <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to bind to.</typeparam>
-	/// <param name="f">The transformer function returning a new result to execute on the inner value.</param>
-	/// <returns>The <see cref="Result{T}"/> returned by <paramref name="f"/>.</returns>
-	public static async Task<Result<U>> BindResultAsync<T, U>(this Task<Result<T>> source, Func<T, Result<U>> f) =>
-		(await source).Bind(f);
-	/// <summary>
-	/// Asynchronously binds the inner value of the result of a value task evaluating to a <see cref="Result{T}"/> to a new <see cref="Result{T}"/>.
-	/// </summary>
-	/// <typeparam name="U">The type of the new value to bind to.</typeparam>
-	/// <param name="f">The transformer function returning a new result to execute on the inner value.</param>
-	/// <returns>The <see cref="Result{T}"/> returned by <paramref name="f"/>.</returns>
-	public static async Task<Result<U>> BindResultAsync<T, U>(this ValueTask<Result<T>> source, Func<T, Result<U>> f) =>
-		(await source).Bind(f);
-
-	/// <summary>
-	/// Returns a <see cref="Result{T}"/> with a value depending on whether the inner value
-	/// of the source is <see langword="null"/>.
-	/// </summary>
-	/// <param name="error">The error message is the inner value is null.</param>
-	public static Result<T> NotNull<T>(this Result<T?> result, string? error = null)
-	where T : class =>
-		result.Bind(v => v is not null ? new Result<T>(v) : error ?? "Inner result value was null");
-	/// <summary>
-	/// Returns a <see cref="Result{T}"/> with a value depending on whether the inner value
-	/// of the source is <see langword="null"/>.
-	/// </summary>
-	/// <param name="error">The error message is the inner value is null.</param>
-	public static Result<T> NotNull<T>(this Result<T?> result, string? error = null)
-	where T : struct =>
-		result.Bind(v => v is not null ? new Result<T>(v.Value) : error ?? "Inner result value was null");
-	public static async Task<Result<T>> NotNullResultAsync<T>(this Task<Result<T?>> result, string? error = null)
-	where T : class =>
-		(await result).Bind(v => v is not null ? new Result<T>(v) : error ?? "Inner result value was null");
-	public static async Task<Result<T>> NotNullResultAsync<T>(this Task<Result<T?>> result, string? error = null)
-	where T : struct =>
-		(await result).Bind(v => v is not null ? new Result<T>(v.Value) : error ?? "Inner result value was null");
-
-	public static IEnumerable<T> WhereSuccessful<T>(this IEnumerable<Result<T>> source) =>
-		source
-			.Where(e => e.IsSuccess)
-			.Select(e => e.Value!);
 
 }
---
+++ src/NanopassSharp/Functional/ResultExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace NanopassSharp.Functional;
+public static class ResultExtensions {
+
+	/// <summary>
+	/// Returns the inner value as either itself or the type's default value.
+	/// </summary>
+	public static T? ToMaybeDefault<T>(this Result<T> source) =>
+		source.IsSuccess
+			? source.Value
+			: default;
+
+	/// <summary>
+	/// Returns a <see cref="Result{T}"/> with a value depending on whether the inner value
+	/// of the source is <see langword="null"/>.
+	/// </summary>
+	/// <param name="error">The error message is the inner value is null.</param>
+	public static Result<T> NotNull<T>(this Result<T?> result, string? error = null)
+	where T : class =>
+		result.Bind(v => v is not null ? new Result<T>(v) : error ?? "Inner result value was null");
+	/// <summary>
+	/// Returns a <see cref="Result{T}"/> with a value depending on whether the inner value
+	/// of the source is <see langword="null"/>.
+	/// </summary>
+	/// <param name="error">The error message is the inner value is null.</param>
+	public static Result<T> NotNull<T>(this Result<T?> result, string? error = null)
+	where T : struct =>
+		result.Bind(v => v is not null ? new Result<T>(v.Value) : error ?? "Inner result value was null");
+
+}
