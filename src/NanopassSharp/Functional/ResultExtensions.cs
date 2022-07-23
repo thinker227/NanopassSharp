4ff942c8acab0b893fcb6ee868bd299bf84657bd
thinker227 <thinker0227@gmail.com>
2022-07-24 01:13:26 +0200

    Added Apply extension... because applicative monads

--- src/NanopassSharp/Functional/ResultExtensions.cs
+++ src/NanopassSharp/Functional/ResultExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace NanopassSharp.Functional;
+﻿using System;
+
+namespace NanopassSharp.Functional;
 public static class ResultExtensions {
 
 	/// <summary>
@@ -25,5 +27,17 @@ public static class ResultExtensions {
 	public static Result<T> NotNull<T>(this Result<T?> result, string? error = null)
 	where T : struct =>
 		result.Bind(v => v is not null ? new Result<T>(v.Value) : error ?? "Inner result value was null");
+
+	/// <summary>
+	/// Applies the inner function of a <see cref="Result{T}"/>
+	/// to the inner value of another <see cref="Result{T}"/>.
+	/// </summary>
+	/// <param name="result">The result which inner value to apply the function to.</param>
+	public static Result<U> Apply<T, U>(this Result<Func<T, U>> source, Result<T> result) =>
+		source.Bind(f =>
+			result.Bind(x =>
+				new Result<U>(f(x))
+			)
+		);
 
 }
