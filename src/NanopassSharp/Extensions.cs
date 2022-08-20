52bbd0821a1e70cf768a062382f939f260e8a1a4
thinker227 <thinker0227@gmail.com>
2022-08-20 16:37:49 +0200

    Add... tons of extensions

--- src/NanopassSharp/Extensions.cs
+++ src/NanopassSharp/Extensions.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -51,4 +53,55 @@ public static class Extensions
         var result = await task;
         return await f(result);
     }
+
+    public static bool DictionaryEquals<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> other) =>
+        source.DictionaryEquals(other, EqualityComparer<TValue>.Default);
+    public static bool DictionaryEquals<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> other, IEqualityComparer<TValue> valueComparer) =>
+        DictionaryEquals(new ReadOnlyDictionaryAdapter<TKey, TValue>(source), new ReadOnlyDictionaryAdapter<TKey, TValue>(other), valueComparer);
+    public static bool DictionaryEquals<TKey, TValue>(this IDictionary<TKey, TValue> source, IReadOnlyDictionary<TKey, TValue> other) =>
+        source.DictionaryEquals(other, EqualityComparer<TValue>.Default);
+    public static bool DictionaryEquals<TKey, TValue>(this IDictionary<TKey, TValue> source, IReadOnlyDictionary<TKey, TValue> other, IEqualityComparer<TValue> valueComparer) =>
+        DictionaryEquals(new ReadOnlyDictionaryAdapter<TKey, TValue>(source), other, valueComparer);
+    public static bool DictionaryEquals<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source, IDictionary<TKey, TValue> other) =>
+        source.DictionaryEquals(other, EqualityComparer<TValue>.Default);
+    public static bool DictionaryEquals<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source, IDictionary<TKey, TValue> other, IEqualityComparer<TValue> valueComparer) =>
+        DictionaryEquals(source, new ReadOnlyDictionaryAdapter<TKey, TValue>(other), valueComparer);
+    public static bool DictionaryEquals<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source, IReadOnlyDictionary<TKey, TValue> other) =>
+        source.DictionaryEquals(other, EqualityComparer<TValue>.Default);
+    public static bool DictionaryEquals<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> source, IReadOnlyDictionary<TKey, TValue> other, IEqualityComparer<TValue> valueComparer)
+    {
+        if (source.Count != other.Count) return false;
+
+        foreach (var (key, sourceValue) in source)
+        {
+            if (!other.ContainsKey(key)) return false;
+
+            var otherValue = other[key];
+            if (!valueComparer.Equals(sourceValue, otherValue)) return false;
+        }
+
+        return true;
+    }
+    private sealed class ReadOnlyDictionaryAdapter<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+    {
+        private readonly IDictionary<TKey, TValue> dictionary;
+
+        public TValue this[TKey key] => dictionary[key];
+        public IEnumerable<TKey> Keys => dictionary.Keys;
+        public IEnumerable<TValue> Values => dictionary.Values;
+        public int Count => dictionary.Count;
+
+        public ReadOnlyDictionaryAdapter(IDictionary<TKey, TValue> dictionary)
+        {
+            this.dictionary = dictionary;
+        }
+
+        public bool ContainsKey(TKey key) => dictionary.ContainsKey(key);
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => dictionary.GetEnumerator();
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value) => dictionary.TryGetValue(key, out value);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public static bool SetEquals<T>(this ISet<T> source, ISet<T> other) =>
+        source.Count == other.Count && source.Overlaps(other);
 }
