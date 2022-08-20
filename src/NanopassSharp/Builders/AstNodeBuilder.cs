1951b8986a6680bb37f1c00da2b213123bbc18ec
thinker227 <thinker0227@gmail.com>
2022-08-21 00:10:08 +0200

    Add builders

---
+++ src/NanopassSharp/Builders/AstNodeBuilder.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NanopassSharp.Builders;
+
+/// <summary>
+/// A builder for an <see cref="AstNode"/>.
+/// </summary>
+public sealed class AstNodeBuilder
+{
+    private AstNodeBuilder? parent;
+    private readonly Dictionary<string, AstNodeBuilder> childrenBuilders;
+    private readonly Dictionary<string, AstNodeMemberBuilder> memberBuilders;
+
+    /// <summary>
+    /// <inheritdoc cref="AstNode.Name" path="/summary"/>
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// <inheritdoc cref="AstNode.Documentation" path="/summary"/>
+    /// </summary>
+    public string? Documentation { get; set; }
+    /// <summary>
+    /// <inheritdoc cref="AstNode.Parent" path="/summary"/>
+    /// </summary>
+    /// <remarks>This property builds the entire node in order to retrieve the parent.</remarks>
+    public AstNode? Parent => Build().Parent;
+    /// <summary>
+    /// <inheritdoc cref="AstNode.Children" path="/summary"/>
+    /// </summary>
+    public IReadOnlyDictionary<string, AstNode> Children =>
+        Build().Children;
+    /// <summary>
+    /// <inheritdoc cref="AstNode.Members" path="/summary"/>
+    /// </summary>
+    public IReadOnlyDictionary<string, AstNodeMember> Members =>
+        memberBuilders.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Build());
+    /// <summary>
+    /// <inheritdoc cref="AstNode.Attributes" path="/summary"/>
+    /// </summary>
+    public ISet<object> Attributes { get; set; }
+
+
+
+    /// <summary>
+    /// Initializes a new <see cref="AstNodeBuilder"/> instance.
+    /// </summary>
+    /// <param name="name">The name of the node.</param>
+    public AstNodeBuilder(string name)
+    {
+        Name = name;
+        Documentation = null;
+        parent = null;
+        childrenBuilders = new();
+        memberBuilders = new();
+        Attributes = new HashSet<object>();
+    }
+    //public AstNodeBuilder(AstNode node)
+    //{
+    //    Name = node.Name;
+    //    Documentation = node.Documentation;
+    //    var members = node.Members
+    //        .Select(kvp => new KeyValuePair<string, AstNodeMemberBuilder>(kvp.Key, new AstNodeMemberBuilder(kvp.Value)));
+    //    memberBuilders = new Dictionary<string, AstNodeMemberBuilder>(members);
+    //    Attributes = new HashSet<object>(node.Attributes);
+    //}
+
+
+
+    /// <summary>
+    /// Sets the name of the node.
+    /// </summary>
+    /// <param name="name"><inheritdoc cref="Name" path="/summary"/></param>
+    /// <returns>The current builder.</returns>
+    public AstNodeBuilder WithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+    /// <summary>
+    /// Sets the documentation of the node.
+    /// </summary>
+    /// <param name="documentation"><inheritdoc cref="Documentation" path="/summary"/></param>
+    /// <returns>The current builder.</returns>
+    public AstNodeBuilder WithDocumentation(string? documentation)
+    {
+        Documentation = documentation;
+        return this;
+    }
+    /// <summary>
+    /// Adds a child to the node.
+    /// </summary>
+    /// <param name="name">The name of the child.</param>
+    /// <returns>A new builder for the child node.</returns>
+    public AstNodeBuilder AddChild(string name)
+    {
+        AstNodeBuilder nodeBuilder = new(name)
+        {
+            parent = this
+        };
+        childrenBuilders.Add(name, nodeBuilder);
+        return nodeBuilder;
+    }
+    /// <summary>
+    /// Adds a member to the node.
+    /// </summary>
+    /// <param name="member">The member to add.</param>
+    /// <returns>The current builder.</returns>
+    public AstNodeBuilder AddMember(AstNodeMember member)
+    {
+        memberBuilders.Add(member.Name, new AstNodeMemberBuilder(member));
+        return this;
+    }
+    /// <summary>
+    /// Adds a member to the node.
+    /// </summary>
+    /// <param name="name">The name of the member.</param>
+    /// <param name="documentation">The documentation of the member.</param>
+    /// <param name="type">The type of the member.</param>
+    /// <returns>A new builder for the member.</returns>
+    public AstNodeMemberBuilder AddMember(string name, string? documentation = null, string? type = null)
+    {
+        var memberBuilder = new AstNodeMemberBuilder(name).WithDocumentation(documentation).WithType(type);
+        memberBuilders.Add(name, memberBuilder);
+        return memberBuilder;
+    }
+    /// <summary>
+    /// Adds an attribute to the node.
+    /// </summary>
+    /// <param name="attribute">The attribute to add.</param>
+    /// <returns>The current builder.</returns>
+    public AstNodeBuilder AddAttribute(object attribute)
+    {
+        Attributes.Add(attribute);
+        return this;
+    }
+
+    /// <summary>
+    /// Builds the current node "upwards", primarily building the parent node, followed by child nodes.
+    /// Any child builders with the same name as <paramref name="child"/> are ignored.
+    /// In successive recursive calls, <paramref name="child"/> is always null.
+    /// </summary>
+    private (AstNode node, Dictionary<string, AstNode> children) BuildUp(AstNodeBuilder? child)
+    {
+        var parentNode = parent?.BuildUp(this).node;
+        Dictionary<string, AstNode> children = new();
+        AstNode node = new(Name, Documentation, parentNode, children, Members, new HashSet<object>(Attributes));
+        foreach (var (name, builder) in childrenBuilders)
+        {
+            if (name == child?.Name) continue;
+            children.Add(name, builder.BuildDown(node));
+        }
+        return (node, children);
+    }
+    /// <summary>
+    /// Builds the current node "downwards", only building children as a parent node is already provided.
+    /// </summary>
+    private AstNode BuildDown(AstNode? parent)
+    {
+        Dictionary<string, AstNode> children = new();
+        AstNode node = new(Name, Documentation, parent, children, Members, new HashSet<object>(Attributes));
+        foreach (var (name, builder) in childrenBuilders)
+        {
+            children.Add(name, builder.BuildDown(node));
+        }
+        return node;
+    }
+    /// <summary>
+    /// Builds an <see cref="AstNode"/> from the builder.
+    /// </summary>
+    public AstNode Build()
+    {
+        var parentNode = parent?.BuildUp(this);
+        var node = BuildDown(parentNode?.node);
+        parentNode?.children.Add(node.Name, node);
+        return node;
+    }
+    /// <summary>
+    /// Implicitly converts an <see cref="AstNodeBuilder"/>
+    /// into an <see cref="AstNode"/>
+    /// by calling <see cref="Build"/>.
+    /// </summary>
+    /// <param name="builder">The source builder.</param>
+    public static implicit operator AstNode(AstNodeBuilder builder) =>
+        builder.Build();
+}
---
+++ src/NanopassSharp/Builders/AstNodeHierarchyBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NanopassSharp.Builders;
+
+/// <summary>
+/// A builder for an <see cref="AstNodeHierarchy"/>.
+/// </summary>
+public sealed class AstNodeHierarchyBuilder
+{
+    private readonly List<AstNodeBuilder> rootBuilders;
+
+    /// <summary>
+    /// <inheritdoc cref="AstNodeHierarchy.Roots"/>
+    /// </summary>
+    public IEnumerable<AstNode> Roots =>
+        rootBuilders.Select(b => b.Build());
+
+
+
+    /// <summary>
+    /// Initializes a new <see cref="AstNodeHierarchyBuilder"/> instance.
+    /// </summary>
+    public AstNodeHierarchyBuilder()
+    {
+        rootBuilders = new();
+    }
+
+
+
+    //public AstNodeHierarchyBuilder AddRoot(AstNode root)
+    //{
+    //    rootBuilders.Add(new AstNodeBuilder(root));
+    //    return this;
+    //}
+    /// <summary>
+    /// Adds a root node to the hierarchy.
+    /// </summary>
+    /// <param name="name">The name of the node.</param>
+    /// <param name="documentation">The documentation of the node.</param>
+    /// <returns>A new builder for the node.</returns>
+    public AstNodeBuilder AddRoot(string name, string? documentation = null)
+    {
+        var nodeBuilder = new AstNodeBuilder(name).WithDocumentation(documentation);
+        rootBuilders.Add(nodeBuilder);
+        return nodeBuilder;
+    }
+
+    /// <summary>
+    /// Builds an <see cref="AstNodeHierarchy"/> from the builder.
+    /// </summary>
+    public AstNodeHierarchy Build() => new(Roots.ToList());
+    /// <summary>
+    /// Implicitly converts an <see cref="AstNodeBuilder"/> to an <see cref="AstNodeHierarchy"/>.
+    /// </summary>
+    /// <param name="builder">The source builder.</param>
+    public static implicit operator AstNodeHierarchy(AstNodeHierarchyBuilder builder) =>
+        builder.Build();
+}
