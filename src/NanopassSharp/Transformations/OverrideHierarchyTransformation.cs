7711deae1dd5d835d3b27cf84beb987cd4ba9f56
thinker227 <thinker0227@gmail.com>
2022-09-19 22:55:52 +0200

    Add OverrideHierarchyTransformation

---
+++ src/NanopassSharp/Transformations/OverrideHierarchyTransformation.cs
@@ -0,0 +1,24 @@
+﻿namespace NanopassSharp.Transformations;
+
+/// <summary>
+/// A transformation which overrides a hierarchy with a new hierarchy.
+/// </summary>
+public sealed class OverrideHierarchyTransformation : BaseTransformation
+{
+    /// <summary>
+    /// The hierarchy used to override.
+    /// </summary>
+    public AstNodeHierarchy Hierarchy { get; }
+
+    /// <summary>
+    /// Initializes a new <see cref="OverrideHierarchyTransformation"/> instance.
+    /// </summary>
+    /// <param name="hierarchy"><inheritdoc cref="Hierarchy" path="/summary"/></param>
+    public OverrideHierarchyTransformation(AstNodeHierarchy hierarchy)
+    {
+        Hierarchy = hierarchy;
+    }
+
+    public override AstNodeHierarchy ApplyToTree(AstNodeHierarchy tree) =>
+        Hierarchy;
+}
