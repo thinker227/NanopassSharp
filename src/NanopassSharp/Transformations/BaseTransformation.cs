5b3d509f73912f5f72cc5e7cfae75278b4522d99
thinker227 <thinker0227@gmail.com>
2022-09-19 22:55:19 +0200

    Add BaseTransformation

---
+++ src/NanopassSharp/Transformations/BaseTransformation.cs
@@ -0,0 +1,23 @@
+﻿namespace NanopassSharp.Transformations;
+
+/// <summary>
+/// A base implementation of <see cref="ITransformation"/>.
+/// </summary>
+public abstract class BaseTransformation : ITransformation
+{
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Unless overwritten, returns <paramref name="member"/>.
+    /// </remarks>
+    public virtual AstNodeMember ApplyToMember(AstNodeHierarchy tree, AstNode node, AstNodeMember member) => member;
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Unless overwritten, returns <paramref name="node"/>.
+    /// </remarks>
+    public virtual AstNode ApplyToNode(AstNodeHierarchy tree, AstNode node) => node;
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Unless overwritten, returns <paramref name="tree"/>.
+    /// </remarks>
+    public virtual AstNodeHierarchy ApplyToTree(AstNodeHierarchy tree) => tree;
+}
