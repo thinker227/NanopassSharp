ca7c34b2aabcd2d3e43b6813f577cb94bea110cb
thinker227 <thinker0227@gmail.com>
2022-09-19 23:14:20 +0200

    Add unary/binary patterns

---
+++ src/NanopassSharp/Patterns/BinaryPattern.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NanopassSharp.Patterns;
+
+/// <summary>
+/// A pattern which applies a binary operation to two other patterns.
+/// </summary>
+public sealed class BinaryPattern : ITransformationPattern
+{
+    /// <summary>
+    /// The left pattern.
+    /// </summary>
+    public ITransformationPattern Left { get; }
+
+    /// <summary>
+    /// The right pattern.
+    /// </summary>
+    public ITransformationPattern Right { get; }
+
+    /// <summary>
+    /// The binary operation.
+    /// </summary>
+    public Func<bool, bool, bool> Operation { get; }
+
+    public bool IsRecursive =>
+        Left.IsRecursive || Right.IsRecursive;
+
+
+
+    /// <summary>
+    /// Initializes a new <see cref="BinaryPattern"/> instance.
+    /// </summary>
+    /// <param name="left"><inheritdoc cref="Left" path="/summary"/></param>
+    /// <param name="right"><inheritdoc cref="Right" path="/summary"/></param>
+    /// <param name="operation"><inheritdoc cref="Operation" path="/summary"/></param>
+    public BinaryPattern(ITransformationPattern left, ITransformationPattern right, Func<bool, bool, bool> operation)
+    {
+        Left = left;
+        Right = right;
+        Operation = operation;
+    }
+
+
+
+    public bool IsMatch(AstNodeHierarchy tree) =>
+        Operation(Left.IsMatch(tree), Right.IsMatch(tree));
+
+    public bool IsMatch(AstNodeHierarchy tree, AstNode node) =>
+        Operation(Left.IsMatch(tree, node), Right.IsMatch(tree, node));
+
+    public bool IsMatch(AstNodeHierarchy tree, AstNode node, AstNodeMember member) =>
+        Operation(Left.IsMatch(tree, node, member), Right.IsMatch(tree, node, member));
+}
---
+++ src/NanopassSharp/Patterns/TransformationPatternExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace NanopassSharp.Patterns;
+
+public static class TransformationPatternExtensions
+{
+    /// <summary>
+    /// Returns a new <see cref="ITransformationPattern"/> which applies a binary and operation to two patterns
+    /// (<c><paramref name="left"/> &amp;&amp; <paramref name="right"/></c>).
+    /// </summary>
+    /// <param name="left">The left pattern.</param>
+    /// <param name="right">The right pattern.</param>
+    public static ITransformationPattern And(this ITransformationPattern left, ITransformationPattern right) =>
+        new BinaryPattern(left, right, (a, b) => a && b);
+
+    /// <summary>
+    /// Returns a new <see cref="ITransformationPattern"/> which applies a binary or operation to two patterns
+    /// (<c><paramref name="left"/> || <paramref name="right"/></c>).
+    /// </summary>
+    /// <param name="left">The left pattern.</param>
+    /// <param name="right">The right pattern.</param>
+    public static ITransformationPattern Or(this ITransformationPattern left, ITransformationPattern right) =>
+        new BinaryPattern(left, right, (a, b) => a || b);
+
+    /// <summary>
+    /// Returns a new <see cref="ITransformationPattern"/> which applies a binary exclusive or operation to two patterns
+    /// (<c><paramref name="left"/> ^ <paramref name="right"/></c>).
+    /// </summary>
+    /// <param name="left">The left pattern.</param>
+    /// <param name="right">The right pattern.</param>
+    public static ITransformationPattern Xor(this ITransformationPattern left, ITransformationPattern right) =>
+        new BinaryPattern(left, right, (a, b) => a ^ b);
+
+    /// <summary>
+    /// Returns a new <see cref="ITransformationPattern"/> which applies a unary not operation to a pattern.
+    /// (<c>!<paramref name="operand"/></c>).
+    /// </summary>
+    /// <param name="operand">The operand.</param>
+    public static ITransformationPattern Not(this ITransformationPattern operand) =>
+        new UnaryPattern(operand, x => !x);
+}
