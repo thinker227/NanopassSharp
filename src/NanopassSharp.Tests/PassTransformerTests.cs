117588886216550a4023c2c36ac5f075c9e992ab
thinker227 <thinker0227@gmail.com>
2022-09-30 21:42:38 +0200

    Start adding transformer tests

---
+++ src/NanopassSharp.Tests/PassTransformerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NanopassSharp.Builders;
+
+namespace NanopassSharp.Tests;
+
+public class PassTransformerTests
+{
+    [Fact]
+    public void TransformsEmptyTree()
+    {
+        AstNodeHierarchyBuilder builder = new();
+
+        var a = builder.AddRoot("a");
+        var b = a.AddChild("b");
+        b.AddChild("c");
+        b.AddChild("d");
+        a.AddChild("e");
+
+        var replacementTree = builder.Build();
+        var originalTree = new AstNodeHierarchy(Array.Empty<AstNode>());
+
+        var trans = new MockTransformation()
+            .ApplyToTreeReturns(replacementTree);
+        MockTransformationDescription desc = new() { Transformation = trans };
+        PassTransformations transformations = new(new[] { desc });
+
+        var transformedTree = PassTransformer.ApplyTransformations(originalTree, transformations);
+
+        transformedTree.ShouldBe(replacementTree);
+    }
+}
