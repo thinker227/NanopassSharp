f6f7cb2a1633af2d7e7c035b43bc043566cf05ee
thinker227 <thinker0227@gmail.com>
2022-10-01 11:08:34 +0200

    Add documentation to PassExtensions

--- src/NanopassSharp/PassExtensions.cs
+++ src/NanopassSharp/PassExtensions.cs
@@ -6,15 +6,34 @@ namespace NanopassSharp;
 
 public static class PassExtensions
 {
+    /// <summary>
+    /// Gets all nodes of a <see cref="AstNodeHierarchy"/>.
+    /// </summary>
+    /// <param name="tree">The hierarchy to get the nodes of.</param>
+    /// <returns>A collection of nodes.</returns>
     public static IEnumerable<AstNode> GetNodes(this AstNodeHierarchy tree) =>
         tree.Roots.SelectMany(r => r.GetDecendantNodesAndSelf());
 
+    /// <summary>
+    /// Gets the decendant nodes of a node, excluding itself.
+    /// </summary>
+    /// <param name="node">The node to get the decendants of.</param>
+    /// <returns>A collection of decendant nodes.</returns>
     public static IEnumerable<AstNode> GetDecendantNodes(this AstNode node) =>
         node.Children.Values.SelectMany(n => n.GetDecendantNodes());
 
+    /// <summary>
+    /// Gets the decendant nodes of a node, including itself.
+    /// </summary>
+    /// <param name="node">The node to get the decendants of.</param>
+    /// <returns>A collection of decendant nodes and the current node.</returns>
     public static IEnumerable<AstNode> GetDecendantNodesAndSelf(this AstNode node) =>
         node.GetDecendantNodes().Prepend(node);
 
+    /// <summary>
+    /// Gets the root of an <see cref="AstNode"/>.
+    /// </summary>
+    /// <param name="node">The node to get the root of.</param>
     public static AstNode GetRoot(this AstNode node) =>
         node.Parent?.GetRoot() ?? node;
 
@@ -25,6 +44,13 @@ public static class PassExtensions
     public static NodePath GetPath(this AstNode node) =>
         NodePath.Create(node, n => (n.Parent, n.Parent is not null), n => n.Name);
 
+    /// <summary>
+    /// Gets a node in a <see cref="AstNodeHierarchy"/> from a path.
+    /// </summary>
+    /// <param name="hierarchy">The source hierarchy.</param>
+    /// <param name="path">The path to get the node from.</param>
+    /// <returns>The node with the specified path,
+    /// or <see langword="null"/> if the node does not exist.</returns>
     public static AstNode? GetNodeFromPath(this AstNodeHierarchy hierarchy, NodePath path)
     {
         var nodesEnumerator = path.GetNodes().Reverse().GetEnumerator();
