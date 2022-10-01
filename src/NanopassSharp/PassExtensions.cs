c340821aabf6fada2f850bbcc5873fee76ae2689
thinker227 <thinker0227@gmail.com>
2022-10-01 11:02:06 +0200

    Remove tree mutation extensions

--- src/NanopassSharp/PassExtensions.cs
+++ src/NanopassSharp/PassExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 
 namespace NanopassSharp;
@@ -44,73 +43,5 @@ public static class PassExtensions
         }
 
         return currentNode;
-    }
-
-    public static AstNodeHierarchy ReplaceNode(this AstNodeHierarchy tree, AstNode oldNode, AstNode newNode)
-    {
-        var root = oldNode.GetRoot();
-        if (!tree.Roots.Contains(root)) throw new ArgumentException("Node does not exist in the tree", nameof(oldNode));
-
-        var parent = oldNode.Parent;
-        if (parent is null) return tree with
-        {
-            Roots = tree.Roots.ToImmutableArray().Replace(oldNode, newNode)
-        };
-
-        string name = oldNode.Name;
-        var node = newNode with
-        {
-            Name = name
-        };
-        var newParent = parent with
-        {
-            Children = parent.Children.ToImmutableDictionary().SetItem(name, node)
-        };
-        return tree.ReplaceNode(parent, newParent);
-    }
-
-    public static AstNodeHierarchy RemoveNode(this AstNodeHierarchy tree, AstNode node)
-    {
-        var root = node.GetRoot();
-        if (!tree.Roots.Contains(root)) throw new ArgumentException("Node does not exist in the tree", nameof(node));
-
-        var parent = node.Parent;
-        if (parent is null) return tree with
-        {
-            Roots = tree.Roots.ToImmutableArray().Remove(node)
-        };
-
-        string name = node.Name;
-        var newParent = parent with
-        {
-            Children = parent.Children.ToImmutableDictionary().Remove(name)
-        };
-        return tree.ReplaceNode(parent, newParent);
-    }
-    
-    public static AstNode ReplaceMember(this AstNode node, AstNodeMember oldMember, AstNodeMember newMember)
-    {
-        string name = oldMember.Name;
-        if (!node.Members.ContainsKey(name)) throw new ArgumentException("Member does not exist in the node", nameof(oldMember));
-
-        var member = newMember with
-        {
-            Name = name
-        };
-        return node with
-        {
-            Members = node.Members.ToImmutableDictionary().SetItem(name, member)
-        };
-    }
-
-    public static AstNode RemoveMember(this AstNode node, AstNodeMember member)
-    {
-        string name = member.Name;
-        if (!node.Members.ContainsKey(name)) throw new ArgumentException("Member does not exist in the node", nameof(member));
-
-        return node with
-        {
-            Members = node.Members.ToImmutableDictionary().Remove(name)
-        };
     }
 }
