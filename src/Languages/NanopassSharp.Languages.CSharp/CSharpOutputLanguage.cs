6cbabdb8b7daf44f4803a4fcdbfbec69fae7be7f
thinker227 <thinker0227@gmail.com>
2022-10-28 18:03:42 +0200

    Add LanguageHelpers and Languages.CSharp projects

---
+++ src/Languages/NanopassSharp.Languages.CSharp/CSharpOutputLanguage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NanopassSharp.LanguageHelpers;
+
+namespace NanopassSharp.Languages.CSharp;
+
+public sealed class CSharpOutputLanguage : ScribanOutputLanguage
+{
+    public override IEnumerable<string> Aliases { get; } = new[]
+    {
+        "csharp",
+        "cs",
+        "c#",
+    };
+
+    private const string templateResourcePath = "NanopassSharp.Languages.CSharp.template.sbncs";
+
+    public override string GetTemplateString()
+    {
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        using var resourceStream = assembly.GetManifestResourceStream(templateResourcePath);
+
+        if (resourceStream is null)
+        {
+            throw new IOException($"Failed to read resource stream of resource '{templateResourcePath}'.");
+        }
+
+        StreamReader reader = new(resourceStream);
+        string templateString = reader.ReadToEnd();
+
+        return templateString;
+    }
+}
---
+++ src/NanopassSharp.LanguageHelpers/ScribanOutputLanguage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Scriban;
+
+namespace NanopassSharp.LanguageHelpers;
+
+/// <summary>
+/// A base implementation of <see cref="IOutputLanguage"/> using Scriban
+/// to generate text from a template.
+/// </summary>
+public abstract class ScribanOutputLanguage : IOutputLanguage
+{
+    public abstract IEnumerable<string> Aliases { get; }
+
+
+
+    /// <summary>
+    /// Gets the string template to use.
+    /// </summary>
+    /// <remarks>
+    /// The context object provided when rendering the template is an <see cref="AstNodeHierarchy"/>.
+    /// See <see href="https://github.com/scriban/scriban"/> for more information about Scriban.
+    /// </remarks>
+    public abstract string GetTemplateString();
+
+    /// <summary>
+    /// Gets a <see cref="Template"/> from a template string.
+    /// </summary>
+    /// <param name="templateString">The template string to get the <see cref="Template"/> from.</param>
+    public virtual Template GetTemplate(string templateString) =>
+        Template.Parse(templateString);
+
+    public async Task<string> EmitAsync(EmitContext context, CancellationToken cancellationToken)
+    {
+        string templateString = GetTemplateString();
+        var template = GetTemplate(templateString);
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        return await template.RenderAsync(context.Hierarchy);
+    }
+}
