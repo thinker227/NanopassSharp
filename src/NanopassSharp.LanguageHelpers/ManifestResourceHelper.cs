d80c5fb222483d6dcccd78d403bc7cdc6c7d2829
thinker227 <thinker0227@gmail.com>
2022-10-28 23:13:40 +0200

    Fix calling assembly stuff

--- src/NanopassSharp.LanguageHelpers/ManifestResourceHelper.cs
+++ src/NanopassSharp.LanguageHelpers/ManifestResourceHelper.cs
@@ -15,17 +15,8 @@ public static class ManifestResourceHelper
     /// <returns>The resource at the location <paramref name="resourceLocation"/>
     /// within the calling assembly, or <see langword="null"/> if the resource
     /// could not be read.</returns>
-    public static string? ReadResourceAsString(string resourceLocation)
-    {
-        var assembly = Assembly.GetCallingAssembly();
-
-        using var resourceStream = assembly.GetManifestResourceStream(resourceLocation);
-        if (resourceStream is null) return null;
-
-        StreamReader reader = new(resourceStream);
-
-        return reader.ReadToEnd();
-    }
+    public static string? ReadResourceAsString(string resourceLocation) =>
+        ReadResourceAsString(resourceLocation, Assembly.GetCallingAssembly());
 
     /// <summary>
     /// Reads a resource as a string
@@ -36,6 +27,24 @@ public static class ManifestResourceHelper
     /// within the calling assembly.</returns>
     /// <exception cref="IOException"></exception>
     public static string ReadResourceAsStringOrThrow(string resourceLocation) =>
-        ReadResourceAsString(resourceLocation)
+        ReadResourceAsString(resourceLocation, Assembly.GetCallingAssembly())
             ?? throw new IOException($"Failed to read manifest resource '{resourceLocation}'.");
+
+    /// <summary>
+    /// Reads a resource as a string.
+    /// </summary>
+    /// <param name="resourceLocation">The location of the resource.</param>
+    /// <param name="assembly">The assembly in which the resource is located.</param>
+    /// <returns>The resource at the location <paramref name="resourceLocation"/>
+    /// within <paramref name="assembly"/>, or <see langword="null"/> if the resource
+    /// could not be read.</returns>
+    public static string? ReadResourceAsString(string resourceLocation, Assembly assembly)
+    {
+        using var resourceStream = assembly.GetManifestResourceStream(resourceLocation);
+        if (resourceStream is null) return null;
+
+        StreamReader reader = new(resourceStream);
+
+        return reader.ReadToEnd();
+    }
 }
