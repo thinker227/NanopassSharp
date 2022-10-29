e0508392a2a29b21d560bf7a68423a8610a61a62
thinker227 <thinker0227@gmail.com>
2022-10-29 12:12:56 +0200

    Add YAML input language

---
+++ src/Languages/NanopassSharp.Languages.Yaml/YamlInputLanguage.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace NanopassSharp.Languages.Yaml;
+
+public sealed class YamlInputLanguage : IInputLanguage
+{
+    public Task<PassSequence> GeneratePassSequenceAsync(InputContext context, CancellationToken cancellationToken)
+    {
+        
+    }
+}
