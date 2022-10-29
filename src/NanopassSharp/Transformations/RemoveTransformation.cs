8eefc1c0253fa34ff98cbbe691484cde337d43b9
thinker227 <thinker0227@gmail.com>
2022-10-29 23:54:28 +0200

    Add RemoveTransformation

---
+++ src/NanopassSharp/Transformations/RemoveTransformation.cs
@@ -0,0 +1,13 @@
+﻿namespace NanopassSharp.Transformations;
+
+/// <summary>
+/// A transformation which removes nodes and members.
+/// </summary>
+public sealed class RemoveTransformation : BaseTransformation
+{
+    public override AstNode? ApplyToNode(AstNodeHierarchy tree, AstNode node) =>
+        null;
+
+    public override AstNodeMember? ApplyToMember(AstNodeHierarchy tree, AstNode node, AstNodeMember member) =>
+        null;
+}
