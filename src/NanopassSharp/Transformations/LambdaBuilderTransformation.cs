4aba5eb4740f90268ad5b950402ec72064f2e9ee
thinker227 <thinker0227@gmail.com>
2022-11-02 20:32:23 +0100

    Add lambda pattern and transformation(s)

---
+++ src/NanopassSharp/Transformations/LambdaBuilderTransformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NanopassSharp.Builders;
+
+namespace NanopassSharp.Transformations;
+
+public sealed class LambdaBuilderTransformation : ITransformation
+{
+    public Action<AstNodeHierarchy, AstNode, AstNodeMemberBuilder> MemberTransformer { get; init; } =
+        (_, _, _) => { };
+
+    public Action<AstNodeHierarchy, AstNodeBuilder> NodeTransformer { get; init; } =
+        (_, _) => { };
+
+    public Action<AstNodeHierarchyBuilder> TreeTransformer { get; init; } =
+        _ => { };
+
+
+
+    public AstNodeMember? ApplyToMember(AstNodeHierarchy tree, AstNode node, AstNodeMember member)
+    {
+        var builder = AstNodeMemberBuilder.FromMember(member);
+        MemberTransformer(tree, node, builder);
+        return builder.Build();
+    }
+
+    public AstNode? ApplyToNode(AstNodeHierarchy tree, AstNode node)
+    {
+        var builder = new AstNodeHierarchyBuilder().CreateNode(node);
+        NodeTransformer(tree, builder);
+        return builder.Build();
+    }
+
+    public AstNodeHierarchy ApplyToTree(AstNodeHierarchy tree)
+    {
+        var builder = AstNodeHierarchyBuilder.FromHierarchy(tree);
+        TreeTransformer(builder);
+        return builder.Build();
+    }
+}
---
+++ src/NanopassSharp/Transformations/LambdaTransformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NanopassSharp.Transformations;
+
+public sealed class LambdaTransformation : ITransformation
+{
+    public Func<AstNodeHierarchy, AstNode, AstNodeMember, AstNodeMember?> MemberTransformer { get; init; } =
+        (_, _, member) => member;
+
+    public Func<AstNodeHierarchy, AstNode, AstNode?> NodeTransformer { get; init; } =
+        (_, node) => node;
+
+    public Func<AstNodeHierarchy, AstNodeHierarchy> TreeTransformer { get; init; } =
+        hierarchy => hierarchy;
+
+
+
+    public AstNodeMember? ApplyToMember(AstNodeHierarchy tree, AstNode node, AstNodeMember member) =>
+        MemberTransformer(tree, node, member);
+
+    public AstNode? ApplyToNode(AstNodeHierarchy tree, AstNode node) =>
+        NodeTransformer(tree, node);
+
+    public AstNodeHierarchy ApplyToTree(AstNodeHierarchy tree) =>
+        TreeTransformer(tree);
+}
