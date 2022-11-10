eee8daaa6ff2b58a65d973910fb05a8a7fe0a99c
thinker227 <thinker0227@gmail.com>
2022-11-10 20:31:18 +0100

    Add InterfaceAttribute

--- src/Languages/NanopassSharp.Languages.Yaml/Models/AttributeModel.cs
+++ src/Languages/NanopassSharp.Languages.Yaml/Models/AttributeModel.cs
@@ -4,5 +4,7 @@ namespace NanopassSharp.Languages.Yaml.Models;
 
 internal sealed class AttributeModel
 {
-    public AccessibilityAttribute Accessibility { get; init; }
+    public AccessibilityAttribute? Accessibility { get; init; }
+
+    public string? Interface { get; init; }
 }
